a64c8ea59290a9c38fe731afcc7ae901098e7334
Noeasyboy007 <aritrabera69@gmail.com>
2025-03-23 22:22:23 +0530

    cleark-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Click me</Button>
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
